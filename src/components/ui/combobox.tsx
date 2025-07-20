b89a1c840a5a80cb6bacde92b74314dbe20ac65b
Leandro Placona <lplacona@bedtime.com.ar>
2025-07-20 03:15:09 +0000

    scan ean no funciona, no esta abriendo el modulo, solucionalo

--- src/components/ui/combobox.tsx
+++ src/components/ui/combobox.tsx
@@ -161,3 +161,5 @@ const CommandItem = React.forwardRef<
 ))
 
 CommandItem.displayName = CommandPrimitive.Item.displayName
+
+export { Command, CommandInput, CommandList, CommandEmpty, CommandGroup, CommandItem }
